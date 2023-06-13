8ba6802064a734c402a31f400b5bd17345ab0209
Vladislav Kheynov <lcastory86@gmail.com>
2023-06-13 16:54:56 +0700

    feat: create main screen fragment chore: move MainActivity

---
+++ app/src/main/java/ru/kheynov/todoappyandex/presentation/todos/MainScreenFragment.kt
@@ -0,0 +1,50 @@
+package ru.kheynov.todoappyandex.presentation.todos
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.kheynov.todoappyandex.databinding.FragmentMainScreenBinding
+import ru.kheynov.todoappyandex.mainscreen.presentation.MainScreenViewModel
+
+
+class MainScreenFragment : Fragment() {
+    private val viewModel: MainScreenViewModel by viewModels()
+    
+    private var _binding: FragmentMainScreenBinding? = null
+    private val binding get() = _binding!!
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentMainScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fabAddTodo.setOnClickListener {
+            //TODO
+        }
+        binding.rvTodoList.adapter = TodoListAdapter(
+            onTodoCheckboxClick = { todoItem ->
+                //TODO
+            },
+            onTodoLongClick = { todoItem ->
+                //TODO
+            }
+        )
+        binding.rvTodoList.layoutManager = LinearLayoutManager(requireContext())
+    }
+    
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    
+    
+}
---
+++ app/src/main/java/ru/kheynov/todoappyandex/presentation/todos/MainScreenViewModel.kt
@@ -0,0 +1,7 @@
+package ru.kheynov.todoappyandex.mainscreen.presentation
+
+import androidx.lifecycle.ViewModel
+
+class MainScreenViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
