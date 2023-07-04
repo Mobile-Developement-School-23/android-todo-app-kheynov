b135bc48b0b09945c8d90fda722edfd9f8ee2fee
Vladislav Kheynov <lcastory86@gmail.com>
2023-07-04 23:49:13 +0700

    refactor: simplify bind function in adapter

--- app/src/main/java/ru/kheynov/todoappyandex/featureTodosList/presentation/TodoListAdapter.kt
+++ app/src/main/java/ru/kheynov/todoappyandex/featureTodosList/presentation/TodoListAdapter.kt
@@ -9,9 +9,9 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import ru.kheynov.todoappyandex.R
-import ru.kheynov.todoappyandex.databinding.TodosListItemBinding
 import ru.kheynov.todoappyandex.core.domain.entities.TodoItem
 import ru.kheynov.todoappyandex.core.domain.entities.TodoUrgency
+import ru.kheynov.todoappyandex.databinding.TodosListItemBinding
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
@@ -39,10 +39,6 @@ class TodoListAdapter(
     
     override fun getItemCount(): Int = currentList.size
     
-    /**
-     * Todo view holder
-     * @property binding [TodosListItemBinding] - binding
-     */
     inner class TodoViewHolder(private val binding: TodosListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         
@@ -50,69 +46,80 @@ class TodoListAdapter(
          * Binds todo item to view holder
          * @param todo [TodoItem] - todo item
          */
-        @Suppress("CyclomaticComplexMethod")
         fun bind(todo: TodoItem) {
             binding.apply {
                 root.setOnLongClickListener {
                     onTodoLongClick(todo)
                     true
                 }
-                with(itemText) {
-                    paintFlags = if (todo.isDone) {
-                        paintFlags or STRIKE_THRU_TEXT_FLAG
-                    } else {
-                        paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
-                    }
-                    text = todo.text
-                }
-                
-                with(dateText) {
-                    visibility = if (todo.deadline == null) View.GONE else View.VISIBLE
-                    val dateTime = todo.deadline
-                    if (dateTime != null) {
-                        text = itemView.context.getString(
-                            R.string.make_until_placeholder,
-                            dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG))
-                        )
-                    }
-                }
-                
-                with(checkBox) {
-                    isChecked = todo.isDone
-                    setOnClickListener { onTodoCheckboxClick(todo, !todo.isDone) }
-                }
-                
                 infoIcon.setOnClickListener {
                     onTodoDetailsClick(todo)
                 }
-                
-                with(urgencyIndicator) {
-                    visibility = when (todo.urgency) {
-                        TodoUrgency.LOW -> View.VISIBLE
-                        TodoUrgency.STANDARD -> View.INVISIBLE
-                        TodoUrgency.HIGH -> View.VISIBLE
-                    }
-                    setImageDrawable(
-                        when (todo.urgency) {
-                            TodoUrgency.LOW, TodoUrgency.STANDARD -> ContextCompat.getDrawable(
-                                context,
-                                R.drawable.baseline_arrow_downward_24
-                            )
-                            
-                            TodoUrgency.HIGH -> ContextCompat.getDrawable(
-                                context,
-                                R.drawable.ic_high_urgency
-                            )
-                        }
-                    )
-                    setColorFilter(
-                        when (todo.urgency) {
-                            TodoUrgency.LOW -> ContextCompat.getColor(context, R.color.gray)
-                            TodoUrgency.STANDARD -> ContextCompat.getColor(context, R.color.gray)
-                            TodoUrgency.HIGH -> ContextCompat.getColor(context, R.color.red)
-                        }
+                bindTitle(todo)
+                bindDateText(todo)
+                bindCheckbox(todo)
+                bindUrgencyIndicator(todo)
+            }
+        }
+        
+        private fun TodosListItemBinding.bindTitle(todo: TodoItem) {
+            with(itemText) {
+                paintFlags = if (todo.isDone) {
+                    paintFlags or STRIKE_THRU_TEXT_FLAG
+                } else {
+                    paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+                }
+                text = todo.text
+            }
+        }
+        
+        private fun TodosListItemBinding.bindDateText(todo: TodoItem) {
+            with(dateText) {
+                visibility = if (todo.deadline == null) View.GONE else View.VISIBLE
+                val dateTime = todo.deadline
+                if (dateTime != null) {
+                    text = itemView.context.getString(
+                        R.string.make_until_placeholder,
+                        dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG))
                     )
                 }
+            }
+        }
+        
+        private fun TodosListItemBinding.bindCheckbox(todo: TodoItem) {
+            with(checkBox) {
+                isChecked = todo.isDone
+                setOnClickListener { onTodoCheckboxClick(todo, !todo.isDone) }
+            }
+        }
+        
+        private fun TodosListItemBinding.bindUrgencyIndicator(todo: TodoItem) {
+            with(urgencyIndicator) {
+                visibility = when (todo.urgency) {
+                    TodoUrgency.LOW -> View.VISIBLE
+                    TodoUrgency.STANDARD -> View.INVISIBLE
+                    TodoUrgency.HIGH -> View.VISIBLE
+                }
+                setImageDrawable(
+                    when (todo.urgency) {
+                        TodoUrgency.LOW, TodoUrgency.STANDARD -> ContextCompat.getDrawable(
+                            context,
+                            R.drawable.baseline_arrow_downward_24
+                        )
+                        
+                        TodoUrgency.HIGH -> ContextCompat.getDrawable(
+                            context,
+                            R.drawable.ic_high_urgency
+                        )
+                    }
+                )
+                setColorFilter(
+                    when (todo.urgency) {
+                        TodoUrgency.LOW -> ContextCompat.getColor(context, R.color.gray)
+                        TodoUrgency.STANDARD -> ContextCompat.getColor(context, R.color.gray)
+                        TodoUrgency.HIGH -> ContextCompat.getColor(context, R.color.red)
+                    }
+                )
             }
         }
     }
