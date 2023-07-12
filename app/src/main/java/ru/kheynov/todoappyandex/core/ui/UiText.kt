8375cb4e4d24085f410962f864711fce8bc3a584
Vladislav Kheynov <lcastory86@gmail.com>
2023-07-12 23:49:03 +0700

    feat: add compose themes

--- app/src/main/java/ru/kheynov/todoappyandex/core/ui/UiText.kt
+++ app/src/main/java/ru/kheynov/todoappyandex/core/ui/UiText.kt
@@ -1,7 +1,9 @@
-package ru.kheynov.todoappyandex.core.utils
+package ru.kheynov.todoappyandex.core.ui
 
 import android.content.Context
 import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 /**
  * [UiText] is a wrapper for text to decouple it from UI and [Context]
@@ -10,10 +12,18 @@ sealed class UiText {
     data class PlainText(val value: String) : UiText()
     class StringResource(@StringRes val resId: Int) : UiText()
 
-    fun toString(context: Context): String {
+    fun asString(context: Context): String {
         return when (this) {
             is PlainText -> value
             is StringResource -> context.getString(resId)
+        }
+    }
+
+    @Composable
+    fun asString(): String {
+        return when (this) {
+            is PlainText -> value
+            is StringResource -> stringResource(resId)
         }
     }
 }
